a9f8db5958a4560b992abcefaa4e669f9bf78075
Dripa Alexandr <forsyteRun@gmail.com>
2024-01-25 14:05:52 +0200

    feat: Streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import {createReadStream} from 'fs'
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const FILE_TO_READ = 'fileToRead.txt'
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = fileURLToPath(import.meta.url);
+    const pathToDir = dirname(pathToFile);
+    const fileToRead = resolve(pathToDir, 'files', FILE_TO_READ)
+
+    try {
+        const readStream = createReadStream(fileToRead)
+        
+        readStream.on('data', (chunk) => {
+            process.stdout.write(chunk)
+        })
+    } catch (error) {
+        throw new Error(error)
+    }
+
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream'
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback){
+            const transformedChunk = chunk.toString().split('').reverse().join('')
+
+            callback(null, transformedChunk + '\n')
+        }
+    })
+
+    process.stdin
+        .pipe(reverse)
+        .pipe(process.stdout)
+
 };
 
 await transform();
