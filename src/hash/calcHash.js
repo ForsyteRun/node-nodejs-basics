1857fd76f3f1f0156b5ad9eade2c7cf21e67bbe0
Dripa Alexandr <forsyteRun@gmail.com>
2024-01-25 10:30:08 +0200

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Readable } from 'node:stream';
+
+const { createHmac } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const secret = 'abcdefg';
+
+    const pathToFile = fileURLToPath(import.meta.url);
+    const pathToDir = dirname(pathToFile);
+    const file = resolve(pathToDir, 'fileToCalculateHashFor.txt')
+
+    const hash = createHmac('sha256', secret)
+        .update(file)
+        .digest('hex');
+
+    const readStream = Readable.from(hash)
+
+    readStream.on('data', (chunk) => {
+        console.log(chunk);
+    })
 };
 
 await calculateHash();
